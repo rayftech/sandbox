d84fb8a6285cadb91f962f3aa06ab95a9a5526a6
SymOwen <sym.owen@gmail.com>
2024-08-23 01:17:31 +1000

    add basic setup for the project

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
+import { ReactElement } from 'react';
 import './App.css';
+import { NavBar } from '@components/NavBar';
+import { Route, Routes } from 'react-router-dom';
+import ErrorPage from '@pages/ErrorPage';
 
-function App() {
+function App(): ReactElement {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <NavBar />
+      <Routes>
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+      <h1>Home Page</h1>
+      <p>Welcome to the home page</p>
+    </>
   );
 }
 
---
+++ frontend/src/pages/ErrorPage.tsx
@@ -0,0 +1,10 @@
+import { ReactElement } from 'react';
+
+export default function ErrorPage(): ReactElement {
+  return (
+    <div>
+      <h1>404 Error</h1>
+      <p>Page not found</p>
+    </div>
+  );
+}
